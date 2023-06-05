fd42c7efc36bc0ffdfb19a5558527dcf0732e1cb
Miko7845 <Assass1n.kuz@gmail.com>
2023-06-05 15:52:34 +0500

    Make the World Whiz By

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     private bool isOnGround = true;
+    public bool gameOver { get; private set; } = false;
 
     void Start()
     {
@@ -26,6 +27,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
