6b0f2a1e83844d9fd544fba3eac24aa406121e2a
Miko7845 <Assass1n.kuz@gmail.com>
2023-06-12 19:23:49 +0500

    Challenge 4 - Soccer Scripting

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    private SpawnManagerX spawnManager;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+        spawnManager = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+    }
+
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * spawnManager.enemySpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
