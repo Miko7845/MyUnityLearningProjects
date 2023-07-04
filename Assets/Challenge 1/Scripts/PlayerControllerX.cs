d560cf30b1d20c62eb98270d2b4792fb64f34c90
Miko7845 <Assass1n.kuz@gmail.com>
2023-07-04 16:45:00 +0500

    refactoring

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,12 +4,12 @@ public class PlayerControllerX : MonoBehaviour
 {
     [SerializeField] float speed = 12.0f;
     [SerializeField] float rotationSpeed = 25.0f;
-    private float verticalInput;
+    float verticalInput;
 
     void FixedUpdate()
     {
-        verticalInput = Input.GetAxis("Vertical");                                          // get the user's vertical input
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);                      // move the plane forward at a constant rate
-        transform.Rotate(Vector3.left * rotationSpeed * verticalInput * Time.deltaTime);    // tilt the plane up/down based on up/down arrow keys
+        verticalInput = Input.GetAxis("Vertical");                                      
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);                  
+        transform.Rotate(Vector3.left * rotationSpeed * verticalInput * Time.deltaTime);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,14 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 15.0f;
     [SerializeField] float turnSpeed = 12.0f;
-    private float horizontalInput;
-    private float verticalInput;
 
-    void Update()
+    float horizontalInput;
+    float verticalInput;
+
+    void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis("Horizontal"); // -1 to 1
-        verticalInput = Input.GetAxis("Vertical"); // -1 to 1
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
         transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
